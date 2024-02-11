30ffadcf85d0b09b0da2a16b767357ead7cd1a90
Ben Pevsner <git@bpev.me>
2024-02-11 12:23:32 -0600

    feat: move from oak -> hono

--- source/main.tsx
+++ source/main.tsx
@@ -1,5 +1,6 @@
 /* @jsx h */
-import { Application, Router } from "oak";
+import { Hono } from "hono";
+import { serveStatic } from "hono/middleware.ts";
 import { h } from "preact";
 import render from "preact-render-to-string";
 
@@ -9,86 +10,70 @@ import { ROUTE } from "./constants.ts";
 import type { Poem } from "./types.ts";
 
 const { ABOUT, READ, WRITE } = ROUTE;
+const app = new Hono();
 
-const pages = new Router()
-  .get("/", async (ctx) => {
-    try {
-      const { id } = (await getPoem()) || {}; // latest poem
-      if (id) {
-        ctx.response.status = 302;
-        ctx.response.redirect(`/poems/${id}`);
-      } else {
-        ctx.response.status = 403;
-        ctx.response.body = wrap(render(<Client route={WRITE} />));
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  })
-  .get("/poems/:id", async (ctx) => {
-    ctx.response.body = wrap(
-      render(<Client route={READ} poem={await getPoem(ctx.params.id)} />),
-    );
-  })
-  .get("/about", (ctx) => {
-    ctx.response.body = wrap(render(<Client route={ABOUT} />));
-  })
-  .get("/new", (ctx) => {
-    ctx.response.status = 403;
-    ctx.response.body = wrap(render(<Client route={WRITE} />));
+app.get("/", async (c) => {
+  try {
+    const { id } = (await getPoem()) || {}; // latest poem
+    return id
+      ? c.redirect(`/poems/${id}`, 302)
+      : c.html(wrap(render(<Client route={WRITE} />)), 302);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get(
+  "/poems/:id",
+  async (c) =>
+    c.html(
+      wrap(
+        render(<Client route={READ} poem={await getPoem(c.req.param("id"))} />),
+      ),
+    ),
+);
+app.get("/about", (c) => c.html(wrap(render(<Client route={ABOUT} />))));
+app.get("/new", (c) => c.html(wrap(render(<Client route={WRITE} />))));
+
+app.get("/api/poems", async (c) => {
+  const poems: { [key: string]: Poem } = {};
+  for await (const entry of getAllPoems()) {
+    poems[String(entry.key)] = entry.value;
+  }
+  return c.json(poems, 200);
+});
+
+app.get(
+  "/api/poems/:id",
+  async (c) => c.json(await getPoem(c.req.param("id"))),
+);
+
+app.get("/api/poems/:id/delete/:key", async (c) => {
+  if (
+    Deno.env.get("DELETE_KEY") &&
+    Deno.env.get("DELETE_KEY") === c.req.param("key")
+  ) {
+    await deletePoem(c.req.param("id"));
+    return c.text("success!", 200);
+  }
+});
+
+app.post("/api/poems", async (c) => {
+  const body = await c.req.parseBody();
+  if (!body.content) return c.text("No poem content", 400);
+
+  await createPoem({
+    id: null,
+    published: Date.now(),
+    content: String(body.content),
+    author: body.author ? String(body.author) : undefined,
+    title: body.title ? String(body.title) : undefined,
   });
+  return c.redirect("/", 302);
+});
 
-const poems = new Router()
-  // For Export; no expectation of performance
-  .get("/api/poems", async (ctx) => {
-    ctx.response.type = "application/json";
-    const poems: { [key: string]: Poem } = {};
-    for await (const entry of getAllPoems()) {
-      poems[String(entry.key)] = entry.value;
-    }
-    ctx.response.body = JSON.stringify(poems);
-  })
-  .get("/api/poems/:id", async (ctx) => {
-    ctx.response.type = "application/json";
-    ctx.response.body = JSON.stringify(await getPoem(ctx?.params?.id));
-  })
-  .get("/api/poems/:id/delete/:key", async (ctx) => {
-    if (
-      Deno.env.get("DELETE_KEY") &&
-      Deno.env.get("DELETE_KEY") === ctx?.params?.key
-    ) {
-      await deletePoem(ctx?.params?.id);
-      ctx.response.status = 200;
-    }
-  })
-  .post("/api/poems", async (ctx) => {
-    const body = await (ctx.request.body({ type: "form" })).value;
-    const content = body.get("content");
-    if (!content) {
-      ctx.response.status = 400;
-    } else {
-      await createPoem({
-        id: null,
-        published: Date.now(),
-        content,
-        author: body.get("author") || undefined,
-        title: body.get("title") || undefined,
-      });
-      ctx.response.status = 302;
-      ctx.response.redirect("/");
-    }
-  });
+app.get("/index.css", serveStatic({ path: "./public/index.css" }));
+app.get("/manifest.json", serveStatic({ path: "./public/manifest.json" }));
+app.get("/assets/*", serveStatic({ path: "./public/assets" }));
 
-console.log("listening on http://localhost:8080");
-
-await new Application()
-  .use(pages.routes())
-  .use(poems.routes())
-  .use(async (ctx, next) => {
-    try {
-      await ctx.send({ root: `${Deno.cwd()}/public` });
-    } catch {
-      await next();
-    }
-  })
-  .listen({ port: 8080 });
+Deno.serve(app.fetch);
